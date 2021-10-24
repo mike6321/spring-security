7be9e2265beb82fec5d36ea43ecbe68c81faba7e
mike6321 <mike6321@naver.com>
2021-10-24 20:45:54 +0900

    (demo-spring-security-form) 스프링 시큐리티 커스터마이징: JPA 연동

---
+++ demo-spring-security-form/src/main/java/me/choi/demospringsecurityform/account/Account.java
@@ -0,0 +1,29 @@
+package me.choi.demospringsecurityform.account;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Account {
+
+    @Id @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    public void encodePassword(Account account) {
+        this.password = "{noop}" + account.getPassword();
+    }
+}
--- demo-spring-security-form/src/main/java/me/choi/demospringsecurityform/config/SecurityConfig.java
+++ demo-spring-security-form/src/main/java/me/choi/demospringsecurityform/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package me.choi.demospringsecurityform.config;
 
+import me.choi.demospringsecurityform.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,10 +12,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AccountService accountService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .mvcMatchers("/", "/info").permitAll()
+            .mvcMatchers("/", "/info", "/account/**").permitAll()
             .mvcMatchers("/admin").hasRole("ADMIN")
             .anyRequest().authenticated();
         http.formLogin();
@@ -22,8 +27,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("junwoo").password("{noop}123").roles("USER").and()
-                .withUser("admin").password("{noop}123").roles("ADMIN");
+        auth.userDetailsService(accountService);
     }
 }
