2b8ddb91a6cd0ea33eb9aa1a1dbae342a1da6108
mike6321 <mike6321@naver.com>
2021-10-24 21:51:38 +0900

    (demo-spring-security-form) 스프링 시큐리티 테스트 1부

---
+++ demo-spring-security-form/src/test/java/me/choi/demospringsecurityform/account/AccountControllerTest.java
@@ -0,0 +1,51 @@
+package me.choi.demospringsecurityform.account;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithAnonymousUser
+    void index_anonymous() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUser
+    void index_user() throws Exception {
+//        mockMvc.perform(get("/").with(user("user").roles("USER")))
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void admin_user() throws Exception {
+        mockMvc.perform(get("/admin"))
+//        mockMvc.perform(get("/admin").with(user("admin").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
---
+++ demo-spring-security-form/src/test/java/me/choi/demospringsecurityform/account/WithUser.java
@@ -0,0 +1,11 @@
+package me.choi.demospringsecurityform.account;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(username = "junwoo", roles = "USER")
+public @interface WithUser {
+}
