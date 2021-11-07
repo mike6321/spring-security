27d5811a7af266b010e4836e544028167ee4dd06
mike6321 <mike6321@naver.com>
2021-11-07 20:36:11 +0900

    (demo-spring-security-form) 스프링 시큐리티 필터와 FilterChainProxy

--- demo-spring-security-form/src/main/java/me/choi/demospringsecurityform/config/SecurityConfig.java
+++ demo-spring-security-form/src/main/java/me/choi/demospringsecurityform/config/SecurityConfig.java
@@ -3,13 +3,15 @@ package me.choi.demospringsecurityform.config;
 import me.choi.demospringsecurityform.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE - 100)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -18,9 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .mvcMatchers("/", "/info", "/account/**").permitAll()
-            .mvcMatchers("/admin").hasRole("ADMIN")
-            .anyRequest().authenticated();
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
         http.formLogin();
         http.httpBasic();
     }
