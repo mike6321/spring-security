f482a81b7be9b7f4b1c54461ece1157fb2a7f295
mike6321 <mike6321@naver.com>
2021-11-10 08:57:03 +0900

    (demo-spring-security-form) CSRF 토큰 사용 예제

---
+++ demo-spring-security-form/src/test/java/me/choi/demospringsecurityform/account/SignUpControllerTest.java
@@ -0,0 +1,42 @@
+package me.choi.demospringsecurityform.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SignUpControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/signup"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("_csrf")));
+    }
+
+    @Test
+    void processSignUp() throws Exception {
+        mockMvc.perform(post("/signup")
+                .param("username", "mike6321")
+                .param("password", "123")
+                .with(csrf()))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+    }
+}
