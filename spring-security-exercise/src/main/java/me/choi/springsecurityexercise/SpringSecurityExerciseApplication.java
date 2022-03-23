17aa4bde9a9bf9f780540c9aad0fb3c1a6d0a653
mike6321 <mike6321@naver.com>
2022-03-23 22:21:48 +0900

    (demo-spring-security-form) init

---
+++ spring-security-exercise/src/main/java/me/choi/springsecurityexercise/SpringSecurityExerciseApplication.java
@@ -0,0 +1,13 @@
+package me.choi.springsecurityexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityExerciseApplication.class, args);
+    }
+
+}
---
+++ spring-security-exercise/src/test/java/me/choi/springsecurityexercise/SpringSecurityExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.springsecurityexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
