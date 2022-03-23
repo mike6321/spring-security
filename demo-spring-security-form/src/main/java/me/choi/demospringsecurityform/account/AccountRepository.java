7739f29aba1b97bd44caf9b2e54171f3b27f6125
junwoo <mike6321@naver.com>
2022-03-24 08:36:28 +0900

    Delete demo-spring-security-form directory

--- demo-spring-security-form/src/main/java/me/choi/demospringsecurityform/account/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package me.choi.demospringsecurityform.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
-}
