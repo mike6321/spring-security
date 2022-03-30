2609d5f17b37787947bcd20d4ab9d170b7aa2d63
mike6321 <mike6321@naver.com>
2022-03-30 19:17:46 +0900

    (demo-spring-security-form) 스프링 시큐리티 테스트 1부

---
+++ src/test/java/me/choi/account/WithAdmin.java
@@ -0,0 +1,11 @@
+package me.choi.account;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(username = "junwoo", roles = "ADMIN")
+public @interface WithAdmin {
+}
