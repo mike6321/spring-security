d845833db60876bf82fa1e87ace14197da176428
mike6321 <mike6321@naver.com>
2022-04-05 21:58:12 +0900

    (demo-spring-security-form) 로그아웃 처리 필터: LogoutFilter

--- src/main/java/me/choi/config/SecurityConfig.java
+++ src/main/java/me/choi/config/SecurityConfig.java
@@ -45,6 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
 
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
